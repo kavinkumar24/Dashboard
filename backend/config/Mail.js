2e8db7725bd467a62248f7beeaa2b9ad7ee06f6b
kavin <kavinpalanisamy242003@gmail.com>
2024-10-16 15:12:27 +0530

    Udated with secure

--- backend/config/Mail.js
+++ backend/config/Mail.js
@@ -29,33 +29,3 @@
 
 
 
-const nodemailer = require('nodemailer');
-
-// Create a transporter object using the default SMTP transport
-const transporter = nodemailer.createTransport({
-    host: 'smtp.gmail.com', 
-    port: 465, 
-    secure: true, 
-    auth: {
-        user: 'kavinpalanisamy242003@gmail.com', // Your Gmail email address
-        pass: 'Kavinpalanisamy' // Your Gmail password or App Password
-    }
-});
-
-// Set up email data
-const mailOptions = {
-    from: '"KAVIN KUMAR" <kavinpalanisamy242003@gmail.com>', // Sender address
-    to: 'naturalsemart@gmail.com', // List of recipients
-    subject: 'Hello from Node.js', // Subject line
-    text: 'Hello world?', // Plain text body
-    html: '<b>Hello world?</b>' // HTML body
-};
-
-// Send mail
-transporter.sendMail(mailOptions, (error, info) => {
-    if (error) {
-        return console.log(error);
-    }
-    console.log('Message sent: %s', info.messageId);
-});
-
